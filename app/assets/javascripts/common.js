52f9ba5bf6f0cb0e4bff10f0f254000338e68fc7
Ildar Manzhikov <manzhikov@gmail.com>
2012-02-07 18:05:36 +0800

    Design changed

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -4,6 +4,16 @@ var Common = new function() {
       // flash boxes
       $(".notice").fadeOut(5000);
       $(".alert").fadeOut(5000);
+    },
+
+    heightWrapping: function () {
+      var mHeight = $('#wrapper').height();
+      mHeight = mHeight - 142;
+      $('#pageContainer').height(mHeight);
+      arraysHeight = mHeight - 30;
+      $('#leftPillarArray, #rightPillarArray').height(arraysHeight);
+      pillarsHeight = arraysHeight - 193;
+      $('#leftMiddleBottomContainer').height(pillarsHeight);
     }
   };
   return self;
