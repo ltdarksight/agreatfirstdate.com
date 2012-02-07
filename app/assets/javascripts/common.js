86194cbe3795124ecfd45003a7f5cc5b45a16e31
Ildar Manzhikov <manzhikov@gmail.com>
2012-02-07 19:27:53 +0800

    FIX: javascript placeholder fix + toggler for login, Removing cookies after signup

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -4,6 +4,11 @@ var Common = new function() {
       // flash boxes
       $(".notice").fadeOut(5000);
       $(".alert").fadeOut(5000);
+      //Welcome login box toggling
+      $('#login_link').click(function() {
+        $('#welcomeLogin').toggle();
+        return false;
+      });
     },
 
     heightWrapping: function () {
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,27 +2,3 @@ $(function () {
   $(".select_box").selectbox();
 });
 
-$(function() {
-  var passwordField = $('input[type="password"]');
-
-  passwordField.after('<input id="passwordPlaceholder" type="text" value="Password" autocomplete="off" />');
-  var passwordPlaceholder = $('#passwordPlaceholder');
-
-  passwordPlaceholder.show();
-  passwordField.hide();
-
-  passwordPlaceholder.focus(function() {
-    passwordPlaceholder.hide();
-    passwordField.show();
-      passwordField.focus();
-  });
-
-  passwordField.blur(function() {
-    if(passwordField.val() == '') {
-      passwordPlaceholder.show();
-      passwordField.hide();
-    }
-  });
-
-});
-
