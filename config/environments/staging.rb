4637e55fdd92e70a4e262f639a1236656c83dd67
Ildar Manzhikov <manzhikov@gmail.com>
2012-02-08 01:29:09 +0800

    Sendmail on staging server

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,6 @@
 Agreatfirstdate::Application.configure do
   config.action_mailer.delivery_method = :sendmail
+  
+  # Devise mailer needs this option
+  config.action_mailer.default_url_options = { :host => 'agreatfirstdate.rubybakers.com' }
 end
