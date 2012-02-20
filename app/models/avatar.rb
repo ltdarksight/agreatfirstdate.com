5fab4b6a824f102a68056640c2c9d3958e6d9ea9
Ildar Manzhikov <manzhikov@gmail.com>
2012-02-20 20:01:38 +0400

    Fix syntax error

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -39,7 +39,7 @@ class Avatar < ActiveRecord::Base
   def update_image_attributes
     if image.present? && image_changed?
       self.content_type = image.file.content_type
-      self.file_size = file_name.file.size
+      self.file_size = image.file.size
     end
   end
 end
