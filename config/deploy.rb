faa645a11f505015f5b5d7f67e59b5405c36e8a4
Mikhail Dieterle <MikDiet@gmail.com>
2012-04-17 10:39:44 +0800

    modified:   config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ namespace :deploy do
 
   desc "Reload the database with seed data"
   task :seed do
-    run "cd #{current_path} && rake db:seed RAILS_ENV=#{rails_env} --trace"
+    run "cd #{current_path} && rake db:seed RAILS_ENV=#{rails_env}"
   end
 end
 
