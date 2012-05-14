6320aefe91756f8da71658ac299fd9ab6617b0a9
Ildar Manzhikov <manzhikov@gmail.com>
2012-05-15 06:13:55 +0800

    Updated db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120514215515) do
+ActiveRecord::Schema.define(:version => 20120514221029) do
 
   create_table "avatars", :force => true do |t|
     t.integer  "profile_id"
