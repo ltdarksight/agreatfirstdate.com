5fdc6eb267778d8eb96d2a8d795d59f174cbf216
Ildar Manzhikov <manzhikov@gmail.com>
2012-05-16 23:24:29 +0800

    SSL config for staging

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,7 @@
+if Rails.env.staging?
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+end
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
