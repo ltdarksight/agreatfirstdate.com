2ac75a67e4f24343379cb83834e11c4537fcecc1
Ildar Manzhikov <manzhikov@gmail.com>
2012-05-17 05:42:54 +0800

    Changed welcome page

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -4,12 +4,6 @@ var Common = new function() {
       // flash boxes
       $(".alert").delay(5000).fadeOut(2000);
       $(".select_box").selectbox();
-
-      //Welcome toggler
-      $('#login_link').click(function() {
-        $('#welcomeLogin').toggle();
-        return false;
-      });
     }
   };
   return self;
