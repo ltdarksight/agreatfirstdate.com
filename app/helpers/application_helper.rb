961652708dfbc99923204c66553fb875b0d2f666
Mikhail Dieterle <MikDiet@gmail.com>
2012-05-21 21:04:48 +0300

    seo data for homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  
+
   def resource_name
     :user
   end
@@ -29,5 +29,24 @@ module ApplicationHelper
       end
     end
   end
-end
 
+  def seo(data = {})
+    content_for :title, data[:title]
+    content_for :description, data[:description]
+    content_for :keywords, data[:keywords]
+  end
+
+  def seo_description
+    if content_for? :description
+      content_for(:description)
+    end
+  end
+
+  def seo_title
+    if content_for? :title
+      content_for(:title)
+    else
+      t 'seo.title'
+    end
+  end
+end
