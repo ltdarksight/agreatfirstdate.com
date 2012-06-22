930a8981f106f6a1439c316bc7e77164e0ccc0cc
Ildar Manzhikov <manzhikov@gmail.com>
2012-06-22 20:12:59 +0800

    Changed staging url

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,7 +2,7 @@ Agreatfirstdate::Application.configure do
   config.action_mailer.delivery_method = :sendmail
 
   # Devise mailer needs this option
-  config.action_mailer.default_url_options = { :host => 'agreatfirstdate.rubybakers.com' }
+  config.action_mailer.default_url_options = { :host => 'agreatfirstdate.23ninja.com' }
 
   # Code is not reloaded between requests
   config.cache_classes = true
