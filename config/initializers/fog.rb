f4f61601e007d544d45fdd911d7f92c8031c2205
Ildar Manzhikov <manzhikov@gmail.com>
2013-01-24 05:23:36 +0800

    Fix fog initializer

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,7 +2,7 @@ if Settings.carrierwave.storage == 'fog'
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => Settings.s3.access_key
+      :aws_access_key_id      => Settings.s3.access_key,
       :aws_secret_access_key  => Settings.s3.secret_key
     }
     config.fog_directory = Settings.s3.bucket
