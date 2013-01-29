14f41ac6504346dd089d0bc68da432f54ef29122
Ildar Manzhikov <manzhikov@gmail.com>
2013-01-29 16:52:21 +0800

    Fix: adding photo in new event items

--- app/models/event_items_event_photo.rb
+++ app/models/event_items_event_photo.rb
@@ -1,12 +1,4 @@
 class EventItemsEventPhoto < ActiveRecord::Base
   belongs_to :event_item
   belongs_to :event_photo
-
-  validate :photo_owner
-
-  private
-
-  def photo_owner
-    errors[:base] << 'Invalid photo id' unless event_item.pillar.profile == event_photo.profile
-  end
 end
