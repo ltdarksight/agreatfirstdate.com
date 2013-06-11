59e74828cbd7b6913ba5294b32e64927e5820abc
Pechnikov Maxim <parallel588@gmail.com>
2013-06-11 20:29:44 +0400

    disabled asset compress for stging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,7 +20,7 @@ Agreatfirstdate::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
