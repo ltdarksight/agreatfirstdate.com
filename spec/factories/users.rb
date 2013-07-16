6dc3085fd1f4405d09bc39f8dfd18fcaa20d204f
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-07-16 14:14:04 +0300

    Fix seeds
    Seeding was outdated. Fix seeding to make them work.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :user do
     email 'user@rubybakers.com'
     password '123456'
-    after_build { |u| u.skip_confirmation! }
+    after(:build) { |u| u.skip_confirmation! }
+    terms_of_service '1'
   end
 end
