9f8fb24919900cc60af7a4f59991e351dd57d3f7
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-08-19 14:49:39 +0300

    Wrap Point#today scope in lambda

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,6 +1,6 @@
 class Point < ActiveRecord::Base
   attr_accessible :profile, :subject_type, :subject_id, :subject
-  
+
   EVENT_TYPES = {
     'Pillar' => 25,
     'Avatar' => 25,
@@ -12,13 +12,14 @@ class Point < ActiveRecord::Base
     'Session' => 15, #Sign In
     'Profile' => 5 #ProfileView
   }
+
   belongs_to :profile
   belongs_to :subject, polymorphic: true
 
   after_create :increment_profile_points
   after_destroy :decrement_profile_points
 
-  scope :today, where('DATE(points.created_at) = DATE(NOW())')
+  scope :today, -> { where('DATE(points.created_at) = DATE(NOW())') }
 
   def increment_profile_points
     profile.reload
