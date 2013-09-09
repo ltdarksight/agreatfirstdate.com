abe31610e5674060b0c628340aded0da287a63ab
Ildar Manzhikov <manzhikov@gmail.com>
2013-09-09 23:38:43 +0400

    Fixed sign_up tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -4,17 +4,17 @@ feature 'Sign up' do
   scenario 'user can sign up with valid credentials' do
     visit '/users/sign_up'
 
-    within '.devise-block' do
+    within '.reg_popup' do
       within '#new_user' do
         fill_in 'user_email', with: 'user@example.com'
         fill_in 'user_password', with: 'secret'
-        fill_in 'user_password_confirmation', with: 'secret'
-        check 'user_terms_of_service'
-        click_button 'Free sign up'
+        # fill_in 'user_password_confirmation', with: 'secret'
+        # check 'user_terms_of_service'
+        click_button 'Sign up'
       end
     end
 
-    expect(current_path).to eq '/'
-    expect(page).to have_content 'confirmation link has been sent to your email address'
+    expect(current_path).to eq '/me/edit'
+    expect(page).to have_content 'Give us some basic information, then create your profile. You\'ll be searching in less than 2 minutes!'
   end
 end
