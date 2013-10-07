36e55cf52082f546c1ce0384b0ea1c5e6c01da56
Ildar Manzhikov <manzhikov@gmail.com>
2013-10-08 00:59:52 +0400

    Updated schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,7 +18,7 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-if Rails.env.production?
+if ::Rails.env.production?
   every 1.day, :at => '0:00 am' do
     rake "daily_report:create"
   end
